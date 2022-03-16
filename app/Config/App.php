a5703f8f0c6212a5b733339d5a884316140c7499
nouira <nadanouira1999@gmail.com>
2022-03-16 12:22:44 +0100

    confic

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/pfe/public/';
+    public $baseURL = 'http://localhost/pfe2/pfe/public/';
 
     /**
      * --------------------------------------------------------------------------
