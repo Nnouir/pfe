f77cfc0ea207dadd6f8eed4c0940314e1be58438
nouira <nadanouira1999@gmail.com>
2022-03-21 10:22:16 +0100

    up

--- app/Controllers/update.php
+++ app/Controllers/update.php
@@ -2,30 +2,31 @@
 
 namespace App\Controllers;
 
-class envoye extends BaseController
+class update extends BaseController
 {
     public function index()
     {
       $userModel = new \App\Models\OffreModel ();
-         $rj = [
+        $id_offre= $this->request->getVar('id_offre');
+      $rj = [
         'titre'=>  $this->request->getVar('titre'),
         'description' =>  $this->request->getVar('description'),
         'id_rh' =>   session()-> get('id'),  
         'id_resppsych' =>  $this->request->getVar('id_resppsych') ,
         'id_resptech'=>  $this->request->getVar('id_resptech') ,
         'domaine' =>  $this->request->getVar('domaine') , 
-        'duree' =>  $this->request->getVar('duree'), 
-        'dateD'=> date('Y-m-d') ]; 
-         $idoffre = $userModel ->insert($rj);
-                        var_dump( $idoffre );
+        'duree' =>  $this->request->getVar('duree') ]; 
+         $idoffre = $userModel ->update( $id_offre,$rj);
+                        var_dump( $idoffre);
       
        
            
-      $userModel = new \App\Models\NiveauModel   ();
+     $userModel = new \App\Models\NiveauModel   ();
         $arr  =  $this->request->getVar('pointdiplome');
+        $userModel->where('id_offre',$id_offre)->delete();
         foreach ($arr as $id_diplome=> $pdip) {
               $dj = array(
-                'id_offre'=>  $idoffre,
+                'id_offre'=>  $id_offre,
                 'niveau'=> $id_diplome,
                 'points'=>  $pdip);
                $user = $userModel ->insert($dj);     
@@ -36,10 +37,11 @@ class envoye extends BaseController
          
       $userModel = new \App\Models\PlangueModel();
           $arr  =  $this->request->getVar('pointslang');
+          $userModel->where('id_offre',$id_offre)->delete();
           foreach ($arr as $id_langue=> $plang) {
               $lj = array(
               'id_langue'=> $id_langue,          
-              'id_offre'=>  $idoffre,
+              'id_offre'=>  $id_offre,
               'points'=>  $plang );
              $user = $userModel ->insert($lj);
              var_dump( $user); }    
@@ -48,14 +50,15 @@ class envoye extends BaseController
 
               
       $userModel = new \App\Models\SexeModel   ();
+      $userModel->where('id_offre',$id_offre)->delete();
              $array = array(
-                   'id_offre'=>  $idoffre,
+                   'id_offre'=>  $id_offre,
                    'sexe'=>  'M',
                    'points'=>  $this->request->getVar('m'));
                    $user = $userModel ->insert($array);
                    var_dump($array);
                     $sj= array(
-                    'id_offre'=>  $idoffre,
+                    'id_offre'=>  $id_offre,
                     'sexe'=> 'F',
                     'points'=>  $this->request->getVar('f'));
                     $user = $userModel ->insert($sj);
@@ -64,10 +67,11 @@ class envoye extends BaseController
 
       
        $userModel = new \App\Models\CompetenceModel  ();
+       $userModel->where('id_offre',$id_offre)->delete();
           $arra  =  $this->request->getVar('group-a');
          foreach ($arra as $id_competence => $pcom) {
            $cj= array(
-            'id_offre'=>  $idoffre,
+            'id_offre'=>  $id_offre,
             'min'=> $pcom['competence-min'],
             'max'=> $pcom['competence-max'],
             'titre'=> $pcom['competence_titre'],
@@ -78,10 +82,11 @@ class envoye extends BaseController
            
         
       $userModel = new \App\Models\AgeModel   ();
+      $userModel->where('id_offre',$id_offre)->delete();
          $ag  =  $this->request->getVar('group-a');
          foreach ($ag as $id_age => $page) {
            $aj =array(
-            'id_offre'=>  $idoffre,
+            'id_offre'=>  $id_offre,
             'min'=> $page['age-min'],
             'max'=> $page['age-max'],
             'points'=> $page['point']);
@@ -90,16 +95,11 @@ class envoye extends BaseController
                
        
              
-               return redirect()->to(site_url('rh/offre'));     
-               
-                       
-                      
-                     
-                                                
-            
-            
-            
-}//end function
+               return redirect()->to(site_url('rh/offre'));  
+
+
+
+               }//end function
 }//end classe
 
 
